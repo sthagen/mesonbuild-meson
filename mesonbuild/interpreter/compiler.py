mesonbuild/meson

interpreter: remove use of UserFeatureOption
Introduce a dataclass to wrap the result of get_option() for features.
This removes a layering violation, where UserFeatureOptions are created
outside options.py just to apply modifiers like disable_if().

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/interpreter/compiler.py
@@ -25,10 +25,9 @@ from .interpreterobjects import (extract_required_kwarg, extract_search_dirs)
 from .type_checking import INCLUDE_DIRECTORIES, REQUIRED_KW, in_set_validator, NoneType
 
 if T.TYPE_CHECKING:
-    from .. import options
     from ..interpreter import Interpreter
     from ..compilers import Compiler
-    from ..interpreterbase import TYPE_var, TYPE_kwargs
+    from ..interpreterbase import FeatureObject, TYPE_var, TYPE_kwargs
     from .kwargs import ExtractRequired, ExtractSearchDirs
     from .interpreter import SourceOutputs
     from ..mlog import TV_LoggableList
@@ -89,7 +88,7 @@ if T.TYPE_CHECKING:
         header_include_directories: T.List[T.Union[build.IncludeDirs, str]]
         header_no_builtin_args: bool
         header_prefix: str
-        header_required: T.Union[bool, options.UserFeatureOption]
+        header_required: T.Union[bool, FeatureObject]
 
     class PreprocessKW(TypedDict):
         output: str

mesonbuild/interpreter/interpreterobjects.py
@@ -10,7 +10,6 @@ import sys
 from pathlib import Path, PurePath
 
 from .. import mesonlib
-from .. import options
 from .. import build
 from .. import mlog
 
@@ -22,7 +21,8 @@ from ..interpreterbase import (
                                FeatureNew, FeatureDeprecated,
                                typed_pos_args, typed_kwargs, typed_operator,
                                noArgsFlattening, noPosargs, noKwargs, unholder_return,
-                               flatten, resolve_second_level_holders, InterpreterException, InvalidArguments, InvalidCode)
+                               flatten, resolve_second_level_holders, FeatureObject,
+                               InterpreterException, InvalidArguments, InvalidCode)
 from ..interpreter.type_checking import NoneType, ENV_KW, ENV_SEPARATOR_KW, PKGCONFIG_DEFINE_KW
 from ..dependencies import Dependency, ExternalLibrary, InternalDependency
 from ..programs import ExternalProgram, Program
@@ -66,7 +66,7 @@ def extract_required_kwarg(kwargs: 'kwargs.ExtractRequired',
     :param subproject: The subproject this is
     :param feature_check:
         A custom feature check for this use of `required` with a
-        `UserFeatureOption`, defaults to None.
+        `FeatureObject`, defaults to None.
     :param default:
         The default value is `required` is not set in  `kwargs`, defaults to
         True
@@ -77,7 +77,8 @@ def extract_required_kwarg(kwargs: 'kwargs.ExtractRequired',
     """
     val = kwargs.get('required', default)
     required = False
-    if isinstance(val, options.UserFeatureOption):
+    feature: T.Optional[str] = None
+    if isinstance(val, FeatureObject):
         if not feature_check:
             feature_check = FeatureNew('User option "feature"', '0.47.0')
         feature_check.use(subproject)
@@ -110,25 +111,22 @@ def extract_search_dirs(kwargs: 'kwargs.ExtractSearchDirs') -> T.List[str]:
             raise InvalidCode(f'Search directory {d} is not an absolute path.')
     return [str(s) for s in search_dirs]
 
-class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
-    def __init__(self, option: options.UserFeatureOption, interpreter: 'Interpreter'):
+class FeatureOptionHolder(ObjectHolder[FeatureObject]):
+    def __init__(self, option: FeatureObject, interpreter: 'Interpreter'):
         super().__init__(option, interpreter)
         if option and option.is_auto():
-            # TODO: we need to cast here because options is not a TypedDict
-            auto = T.cast('options.UserFeatureOption', self.env.coredata.optstore.resolve_option('auto_features'))
-            self.held_object = copy.copy(auto)
-            self.held_object.name = option.name
+            option.value = T.cast('str', self.env.coredata.optstore.get_value_for('auto_features'))
 
     @property
     def value(self) -> str:
         return 'disabled' if not self.held_object else self.held_object.value
 
-    def as_disabled(self) -> options.UserFeatureOption:
+    def as_disabled(self) -> FeatureObject:
         disabled = copy.deepcopy(self.held_object)
         disabled.value = 'disabled'
         return disabled
 
-    def as_enabled(self) -> options.UserFeatureOption:
+    def as_enabled(self) -> FeatureObject:
         enabled = copy.deepcopy(self.held_object)
         enabled.value = 'enabled'
         return enabled
@@ -158,7 +156,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
     def auto_method(self, args: T.List[TYPE_var], kwargs: TYPE_kwargs) -> bool:
         return self.value == 'auto'
 
-    def _disable_if(self, condition: bool, message: T.Optional[str]) -> options.UserFeatureOption:
+    def _disable_if(self, condition: bool, message: T.Optional[str]) -> FeatureObject:
         if not condition:
             return copy.deepcopy(self.held_object)
 
@@ -176,7 +174,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
         _ERROR_MSG_KW,
     )
     @InterpreterObject.method('require')
-    def require_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> options.UserFeatureOption:
+    def require_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> FeatureObject:
         return self._disable_if(not args[0], kwargs['error_message'])
 
     @FeatureNew('feature_option.disable_if()', '1.1.0')
@@ -186,7 +184,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
         _ERROR_MSG_KW,
     )
     @InterpreterObject.method('disable_if')
-    def disable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> options.UserFeatureOption:
+    def disable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> FeatureObject:
         return self._disable_if(args[0], kwargs['error_message'])
 
     @FeatureNew('feature_option.enable_if()', '1.1.0')
@@ -196,7 +194,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
         _ERROR_MSG_KW,
     )
     @InterpreterObject.method('enable_if')
-    def enable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> options.UserFeatureOption:
+    def enable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> FeatureObject:
         if not args[0]:
             return copy.deepcopy(self.held_object)
 
@@ -211,14 +209,14 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
     @noKwargs
     @typed_pos_args('feature_option.disable_auto_if', bool)
     @InterpreterObject.method('disable_auto_if')
-    def disable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> options.UserFeatureOption:
+    def disable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> FeatureObject:
         return copy.deepcopy(self.held_object) if self.value != 'auto' or not args[0] else self.as_disabled()
 
     @FeatureNew('feature_option.enable_auto_if()', '1.1.0')
     @noKwargs
     @typed_pos_args('feature_option.enable_auto_if', bool)
     @InterpreterObject.method('enable_auto_if')
-    def enable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> options.UserFeatureOption:
+    def enable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> FeatureObject:
         return self.as_enabled() if self.value == 'auto' and args[0] else copy.deepcopy(self.held_object)
 
 

mesonbuild/interpreter/kwargs.py
@@ -14,6 +14,7 @@ from .. import options
 from ..compilers import Compiler
 from ..compilers.compilers import Language
 from ..dependencies.base import Dependency, DependencyMethods, IncludeType
+from ..interpreterbase import FeatureObject
 from ..mesonlib import EnvironmentVariables, MachineChoice, File, FileMode, FileOrString
 from ..options import OptionKey
 from ..modules.cmake import CMakeSubprojectOptions
@@ -80,7 +81,7 @@ class ExtractRequired(TypedDict):
     a boolean or a feature option should inherit its arguments from this class.
     """
 
-    required: T.Union[bool, options.UserFeatureOption]
+    required: T.Union[bool, FeatureObject]
 
 
 class ExtractSearchDirs(TypedDict):

unittests/failuretests.py
@@ -148,7 +148,7 @@ class FailureTests(BasePlatformTests):
         a = (("dependency('zlib', method : 'fail')", 'dependency keyword argument "method" must be one of auto, builtin, cmake, config-tool, cups-config, dub, extraframework, libwmf-config, pcap-config, pkg-config, qmake, sdlconfig, sysconfig, system, not fail'),
              ("dependency('zlib', static : '1')", "dependency keyword argument 'static' was of type str but should have been one of: bool, NoneType"),
              ("dependency('zlib', version : 1)", r"dependency keyword argument 'version' was of type array\[int\] but should have been array\[str\]"),
-             ("dependency('zlib', required : 1)", "dependency keyword argument 'required' was of type int but should have been one of: bool, UserFeatureOption"),
+             ("dependency('zlib', required : 1)", "dependency keyword argument 'required' was of type int but should have been one of: bool, FeatureObject"),
              ("dependency('zlib', method : 1)", "dependency keyword argument 'method' was of type int but should have been str"),
              ("dependency('zlibfail')", self.dnf),)
         for contents, match in a:

sthagen/mesonbuild-meson

mesonbuild/interpreter/compiler.py
@@ -25,10 +25,9 @@ from .interpreterobjects import (extract_required_kwarg, extract_search_dirs)
 from .type_checking import INCLUDE_DIRECTORIES, REQUIRED_KW, in_set_validator, NoneType
 
 if T.TYPE_CHECKING:
-    from .. import options
     from ..interpreter import Interpreter
     from ..compilers import Compiler
-    from ..interpreterbase import TYPE_var, TYPE_kwargs
+    from ..interpreterbase import Feature, TYPE_var, TYPE_kwargs
     from .kwargs import ExtractRequired, ExtractSearchDirs
     from .interpreter import SourceOutputs
     from ..mlog import TV_LoggableList
@@ -89,7 +88,7 @@ if T.TYPE_CHECKING:
         header_include_directories: T.List[T.Union[build.IncludeDirs, str]]
         header_no_builtin_args: bool
         header_prefix: str
-        header_required: T.Union[bool, options.UserFeatureOption]
+        header_required: T.Union[bool, Feature]
 
     class PreprocessKW(TypedDict):
         output: str

mesonbuild/interpreter/interpreterobjects.py
@@ -10,7 +10,6 @@ import sys
 from pathlib import Path, PurePath
 
 from .. import mesonlib
-from .. import options
 from .. import build
 from .. import mlog
 
@@ -22,7 +21,8 @@ from ..interpreterbase import (
                                FeatureNew, FeatureDeprecated,
                                typed_pos_args, typed_kwargs, typed_operator,
                                noArgsFlattening, noPosargs, noKwargs, unholder_return,
-                               flatten, resolve_second_level_holders, InterpreterException, InvalidArguments, InvalidCode)
+                               flatten, resolve_second_level_holders, Feature,
+                               InterpreterException, InvalidArguments, InvalidCode)
 from ..interpreter.type_checking import NoneType, ENV_KW, ENV_SEPARATOR_KW, PKGCONFIG_DEFINE_KW
 from ..dependencies import Dependency, ExternalLibrary, InternalDependency
 from ..programs import ExternalProgram, Program
@@ -66,7 +66,7 @@ def extract_required_kwarg(kwargs: 'kwargs.ExtractRequired',
     :param subproject: The subproject this is
     :param feature_check:
         A custom feature check for this use of `required` with a
-        `UserFeatureOption`, defaults to None.
+        `Feature`, defaults to None.
     :param default:
         The default value is `required` is not set in  `kwargs`, defaults to
         True
@@ -77,7 +77,8 @@ def extract_required_kwarg(kwargs: 'kwargs.ExtractRequired',
     """
     val = kwargs.get('required', default)
     required = False
-    if isinstance(val, options.UserFeatureOption):
+    feature: T.Optional[str] = None
+    if isinstance(val, Feature):
         if not feature_check:
             feature_check = FeatureNew('User option "feature"', '0.47.0')
         feature_check.use(subproject)
@@ -110,25 +111,22 @@ def extract_search_dirs(kwargs: 'kwargs.ExtractSearchDirs') -> T.List[str]:
             raise InvalidCode(f'Search directory {d} is not an absolute path.')
     return [str(s) for s in search_dirs]
 
-class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
-    def __init__(self, option: options.UserFeatureOption, interpreter: 'Interpreter'):
+class FeatureOptionHolder(ObjectHolder[Feature]):
+    def __init__(self, option: Feature, interpreter: 'Interpreter'):
         super().__init__(option, interpreter)
         if option and option.is_auto():
-            # TODO: we need to cast here because options is not a TypedDict
-            auto = T.cast('options.UserFeatureOption', self.env.coredata.optstore.resolve_option('auto_features'))
-            self.held_object = copy.copy(auto)
-            self.held_object.name = option.name
+            option.value = T.cast('str', self.env.coredata.optstore.get_value_for('auto_features'))
 
     @property
     def value(self) -> str:
         return 'disabled' if not self.held_object else self.held_object.value
 
-    def as_disabled(self) -> options.UserFeatureOption:
+    def as_disabled(self) -> Feature:
         disabled = copy.deepcopy(self.held_object)
         disabled.value = 'disabled'
         return disabled
 
-    def as_enabled(self) -> options.UserFeatureOption:
+    def as_enabled(self) -> Feature:
         enabled = copy.deepcopy(self.held_object)
         enabled.value = 'enabled'
         return enabled
@@ -158,7 +156,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
     def auto_method(self, args: T.List[TYPE_var], kwargs: TYPE_kwargs) -> bool:
         return self.value == 'auto'
 
-    def _disable_if(self, condition: bool, message: T.Optional[str]) -> options.UserFeatureOption:
+    def _disable_if(self, condition: bool, message: T.Optional[str]) -> Feature:
         if not condition:
             return copy.deepcopy(self.held_object)
 
@@ -176,7 +174,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
         _ERROR_MSG_KW,
     )
     @InterpreterObject.method('require')
-    def require_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> options.UserFeatureOption:
+    def require_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> Feature:
         return self._disable_if(not args[0], kwargs['error_message'])
 
     @FeatureNew('feature_option.disable_if()', '1.1.0')
@@ -186,7 +184,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
         _ERROR_MSG_KW,
     )
     @InterpreterObject.method('disable_if')
-    def disable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> options.UserFeatureOption:
+    def disable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> Feature:
         return self._disable_if(args[0], kwargs['error_message'])
 
     @FeatureNew('feature_option.enable_if()', '1.1.0')
@@ -196,7 +194,7 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
         _ERROR_MSG_KW,
     )
     @InterpreterObject.method('enable_if')
-    def enable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> options.UserFeatureOption:
+    def enable_if_method(self, args: T.Tuple[bool], kwargs: 'kwargs.FeatureOptionRequire') -> Feature:
         if not args[0]:
             return copy.deepcopy(self.held_object)
 
@@ -211,14 +209,14 @@ class FeatureOptionHolder(ObjectHolder[options.UserFeatureOption]):
     @noKwargs
     @typed_pos_args('feature_option.disable_auto_if', bool)
     @InterpreterObject.method('disable_auto_if')
-    def disable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> options.UserFeatureOption:
+    def disable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> Feature:
         return copy.deepcopy(self.held_object) if self.value != 'auto' or not args[0] else self.as_disabled()
 
     @FeatureNew('feature_option.enable_auto_if()', '1.1.0')
     @noKwargs
     @typed_pos_args('feature_option.enable_auto_if', bool)
     @InterpreterObject.method('enable_auto_if')
-    def enable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> options.UserFeatureOption:
+    def enable_auto_if_method(self, args: T.Tuple[bool], kwargs: TYPE_kwargs) -> Feature:
         return self.as_enabled() if self.value == 'auto' and args[0] else copy.deepcopy(self.held_object)
 
 

mesonbuild/interpreter/kwargs.py
@@ -14,6 +14,7 @@ from .. import options
 from ..compilers import Compiler
 from ..compilers.compilers import Language
 from ..dependencies.base import Dependency, DependencyMethods, IncludeType
+from ..interpreterbase import Feature
 from ..mesonlib import EnvironmentVariables, MachineChoice, File, FileMode, FileOrString
 from ..options import OptionKey
 from ..modules.cmake import CMakeSubprojectOptions
@@ -85,7 +86,7 @@ class ExtractRequired(TypedDict):
     a boolean or a feature option should inherit its arguments from this class.
     """
 
-    required: T.Union[bool, options.UserFeatureOption]
+    required: T.Union[bool, Feature]
 
 
 class ExtractSearchDirs(TypedDict):

unittests/failuretests.py
@@ -148,7 +148,7 @@ class FailureTests(BasePlatformTests):
         a = (("dependency('zlib', method : 'fail')", 'dependency keyword argument "method" must be one of auto, builtin, cmake, config-tool, cups-config, dub, extraframework, libwmf-config, pcap-config, pkg-config, qmake, sdlconfig, sysconfig, system, not fail'),
              ("dependency('zlib', static : '1')", "dependency keyword argument 'static' was of type str but should have been one of: bool, NoneType"),
              ("dependency('zlib', version : 1)", r"dependency keyword argument 'version' was of type array\[int\] but should have been array\[str\]"),
-             ("dependency('zlib', required : 1)", "dependency keyword argument 'required' was of type int but should have been one of: bool, UserFeatureOption"),
+             ("dependency('zlib', required : 1)", "dependency keyword argument 'required' was of type int but should have been one of: bool, Feature"),
              ("dependency('zlib', method : 1)", "dependency keyword argument 'method' was of type int but should have been str"),
              ("dependency('zlibfail')", self.dnf),)
         for contents, match in a: