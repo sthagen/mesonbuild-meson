mesonbuild/meson

More robust handling of linker argument wrapping
Not all compilers (swiftc in case of #14261) concatenate linker
arguments with ','. This removes that assumption and instead always
passes the arguments in a list where applicable, and reworks the
linker argument wrapper syntaxes for the compilers to more accurately
represent the three different option styles.

Some Apple ld options, notably -install_name, are also not exposed
directly on the compiler options interface, hence add the linker
prefix here.

mesonbuild/compilers/compilers.py
@@ -459,6 +459,76 @@ class RunResult(HoldableObject):
 
 
 @dataclass
+class PrefixArgumentLinkerOptionWrapperStyle:
+    """
+    Represents a linker option wrapper style where each linker argument is preceded by a separate argument representing
+    the compiler's passthrough option, like Clang: ['-Xlinker', 'arg1', '-Xlinker', 'arg2'] passes ['arg1', 'arg2'] to the
+    linker.
+    """
+
+    prefix_arg: str
+
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        return [compiler_arg for linker_arg in group for compiler_arg in [self.prefix_arg, linker_arg]]
+
+
+@dataclass
+class SimplePrefixLinkerOptionWrapperStyle:
+    """
+    Represents a linker option wrapper style where each linker argument is prefixed with the compiler's passthrough
+    option as part of the same argument, like DMD: ['-L=arg1', '-L=arg2'] passes ['arg1', 'arg2'] to the linker.
+    """
+
+    prefix: str
+
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        return [self.prefix + linker_arg for linker_arg in group]
+
+
+class LinkerOptionWrapException(MesonException):
+    pass
+
+
+@dataclass
+class ManyInOneLinkerOptionWrapperStyle:
+    """
+    Represents a linker option wrapper style where multiple linker arguments are passed as a single argument to the
+    compiler combined with a prefix, like GCC: ['-Wl,arg1,arg2'] passes ['arg1', 'arg2'] to the linker.
+    """
+
+    prefix: str
+    separator: str
+
+    fallback: T.Optional[LinkerOptionWrapperStyle] = None
+
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        for el in group:
+            if self.separator not in el:
+                continue
+
+            if self.fallback is not None:
+                return self.fallback.wrap(group)
+
+            full = ''
+            if len(group) > 1:
+                full = f' (part of arguments list {group!r})'
+            stripped = f'{self.prefix}{el}'
+            raise LinkerOptionWrapException(f'Cannot wrap linker argument {el!r}{full} for compiler interface: '
+                                            f'would result in {stripped!r}, which the compiler would interpret as '
+                                            f'multiple linker arguments since it treats {self.separator!r} as the '
+                                            'separator.')
+
+        return [self.prefix + self.separator.join(group)]
+
+
+LinkerOptionWrapperStyle = T.Union[
+    PrefixArgumentLinkerOptionWrapperStyle,
+    SimplePrefixLinkerOptionWrapperStyle,
+    ManyInOneLinkerOptionWrapperStyle,
+]
+
+
+@dataclass
 class CompileResult(HoldableObject):
 
     """The result of Compiler.compiles (and friends)."""
@@ -480,7 +550,7 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
     # manually searched.
     internal_libs: T.List[str] = []
 
-    LINKER_PREFIX: T.Union[None, str, T.List[str]] = None
+    LINKER_PREFIX: T.Optional[LinkerOptionWrapperStyle] = None
     INVOKES_LINKER = True
 
     language: Language

mesonbuild/compilers/cuda.py
@@ -10,7 +10,7 @@ import typing as T
 
 from .. import options
 from ..mesonlib import is_windows, LibType, version_compare
-from .compilers import Compiler, CompileCheckMode, CrossNoRunException
+from .compilers import Compiler, CompileCheckMode, CrossNoRunException, SimplePrefixLinkerOptionWrapperStyle
 
 if T.TYPE_CHECKING:
     from ..build import BuildTarget
@@ -45,7 +45,7 @@ class Phase(enum.Enum):
 
 class CudaCompiler(Compiler):
 
-    LINKER_PREFIX = '-Xlinker='
+    LINKER_PREFIX = SimplePrefixLinkerOptionWrapperStyle('-Xlinker=')
     language = 'cuda'
 
     # NVCC flags taking no arguments.

mesonbuild/compilers/mixins/gnu.py
@@ -17,7 +17,7 @@ import typing as T
 from ... import mesonlib
 from ... import mlog
 from ...options import OptionKey, UserStdOption
-from mesonbuild.compilers.compilers import CompileCheckMode
+from mesonbuild.compilers.compilers import CompileCheckMode, ManyInOneLinkerOptionWrapperStyle
 
 if T.TYPE_CHECKING:
     from ..._typing import ImmutableListProtocol
@@ -356,7 +356,7 @@ class GnuLikeCompiler(Compiler, metaclass=abc.ABCMeta):
     that the actual concrete subclass define their own implementation.
     """
 
-    LINKER_PREFIX = '-Wl,'
+    LINKER_PREFIX = ManyInOneLinkerOptionWrapperStyle('-Wl,', ',')
 
     def __init__(self) -> None:
         self.base_options = {

mesonbuild/compilers/mixins/tasking.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import os
 import typing as T
 
+from ...compilers.compilers import SimplePrefixLinkerOptionWrapperStyle
 from ...mesonlib import EnvironmentException
 from ...options import OptionKey
 
@@ -48,7 +49,7 @@ class TaskingCompiler(Compiler):
     Functionality that is common to all TASKING family compilers.
     '''
 
-    LINKER_PREFIX = '-Wl'
+    LINKER_PREFIX = SimplePrefixLinkerOptionWrapperStyle('-Wl')
 
     def __init__(self) -> None:
         if not self.is_cross:

unittests/internaltests.py
@@ -28,6 +28,7 @@ import mesonbuild.modules.gnome
 import mesonbuild.scripts.env2mfile
 from mesonbuild import coredata
 from mesonbuild.compilers.c import ClangCCompiler, GnuCCompiler
+from mesonbuild.compilers.compilers import ManyInOneLinkerOptionWrapperStyle
 from mesonbuild.compilers.cpp import VisualStudioCPPCompiler
 from mesonbuild.compilers.d import DmdDCompiler
 from mesonbuild.compilers.detect import detect_c_compiler
@@ -240,7 +241,7 @@ class InternalTests(unittest.TestCase):
     def test_compiler_args_class_gnuld(self):
         ## Test --start/end-group
         env = get_fake_env()
-        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, '-Wl,', [])
+        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, ManyInOneLinkerOptionWrapperStyle('-Wl,', ','), [])
         gcc = GnuCCompiler([], [], 'fake', MachineChoice.HOST, env, linker=linker)
         ## Ensure that the fake compiler is never called by overriding the relevant function
         gcc.get_default_include_dirs = lambda: ['/usr/include', '/usr/share/include', '/usr/local/include']
@@ -270,7 +271,7 @@ class InternalTests(unittest.TestCase):
     def test_compiler_args_remove_system(self):
         ## Test --start/end-group
         env = get_fake_env()
-        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, '-Wl,', [])
+        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, ManyInOneLinkerOptionWrapperStyle('-Wl,', ','), [])
         gcc = GnuCCompiler([], [], 'fake', MachineChoice.HOST, env, linker=linker)
         ## Ensure that the fake compiler is never called by overriding the relevant function
         gcc.get_default_include_dirs = lambda: ['/usr/include', '/usr/share/include', '/usr/local/include']

sthagen/mesonbuild-meson

mesonbuild/compilers/compilers.py
@@ -454,6 +454,75 @@ class RunResult(HoldableObject):
     cached: bool = False
 
 
+class LinkerOptionStyle(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        ...
+
+
+@dataclass
+class PrefixArgumentLinkerOptionStyle(LinkerOptionStyle):
+    """
+    Represents a linker option style where each linker argument is preceded by a separate argument representing
+    the compiler's pass-through option, like Clang: ['-Xlinker', 'arg1', '-Xlinker', 'arg2'] passes ['arg1', 'arg2'] to the
+    linker.
+    """
+
+    prefix_arg: str
+
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        return [compiler_arg for linker_arg in group for compiler_arg in [self.prefix_arg, linker_arg]]
+
+
+@dataclass
+class SimplePrefixLinkerOptionStyle(LinkerOptionStyle):
+    """
+    Represents a linker option style where each linker argument is prefixed with the compiler's pass-through
+    option as part of the same argument, like DMD: ['-L=arg1', '-L=arg2'] passes ['arg1', 'arg2'] to the linker.
+    """
+
+    prefix: str
+
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        return [self.prefix + linker_arg for linker_arg in group]
+
+
+class LinkerArgumentWrapException(MesonException):
+    pass
+
+
+@dataclass
+class ManyInOneLinkerOptionStyle(LinkerOptionStyle):
+    """
+    Represents a linker option style where multiple linker arguments are passed as a single argument to the
+    compiler combined with a prefix, like GCC: ['-Wl,arg1,arg2'] passes ['arg1', 'arg2'] to the linker.
+    """
+
+    prefix: str
+    separator: str
+
+    fallback: T.Optional[LinkerOptionStyle] = None
+
+    def wrap(self, group: T.List[str]) -> T.List[str]:
+        for el in group:
+            if self.separator not in el:
+                continue
+
+            if self.fallback is not None:
+                return self.fallback.wrap(group)
+
+            full = ''
+            if len(group) > 1:
+                full = f' (part of arguments list {group!r})'
+            stripped = f'{self.prefix}{el}'
+            raise LinkerArgumentWrapException(f'Cannot wrap linker argument {el!r}{full} for compiler interface: '
+                                              f'would result in {stripped!r}, which the compiler would interpret as '
+                                              f'multiple linker arguments since it treats {self.separator!r} as the '
+                                              'separator.')
+
+        return [self.prefix + self.separator.join(group)]
+
+
 @dataclass
 class CompileResult(HoldableObject):
 
@@ -476,7 +545,7 @@ class Compiler(HoldableObject, metaclass=SimpleABC):
     # manually searched.
     internal_libs: T.List[str] = []
 
-    LINKER_PREFIX: T.Union[None, str, T.List[str]] = None
+    LINKER_PREFIX: T.Optional[LinkerOptionStyle] = None
 
     # If the compiler is used to fire a separate linking step, environment
     # variables like CFLAGS have to be passed to the linking step as well.

mesonbuild/compilers/cuda.py
@@ -10,7 +10,7 @@ import typing as T
 
 from .. import options
 from ..mesonlib import is_windows, LibType, version_compare
-from .compilers import Compiler, CompileCheckMode, CrossNoRunException
+from .compilers import Compiler, CompileCheckMode, CrossNoRunException, SimplePrefixLinkerOptionStyle
 
 if T.TYPE_CHECKING:
     from ..build import BuildTarget
@@ -45,7 +45,7 @@ class Phase(enum.Enum):
 
 class CudaCompiler(Compiler):
 
-    LINKER_PREFIX = '-Xlinker='
+    LINKER_PREFIX = SimplePrefixLinkerOptionStyle('-Xlinker=')
     language = 'cuda'
 
     # NVCC flags taking no arguments.

mesonbuild/compilers/mixins/gnu.py
@@ -17,7 +17,7 @@ import typing as T
 from ... import mesonlib
 from ... import mlog
 from ...options import OptionKey, UserStdOption
-from mesonbuild.compilers.compilers import CompileCheckMode
+from mesonbuild.compilers.compilers import CompileCheckMode, ManyInOneLinkerOptionStyle
 
 if T.TYPE_CHECKING:
     from ..._typing import ImmutableListProtocol
@@ -356,7 +356,7 @@ class GnuLikeCompiler(Compiler, metaclass=mesonlib.SimpleABC):
     that the actual concrete subclass define their own implementation.
     """
 
-    LINKER_PREFIX = '-Wl,'
+    LINKER_PREFIX = ManyInOneLinkerOptionStyle('-Wl,', ',')
 
     def __init__(self) -> None:
         self.base_options = {

mesonbuild/compilers/mixins/tasking.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import os
 import typing as T
 
+from ...compilers.compilers import SimplePrefixLinkerOptionStyle
 from ...mesonlib import EnvironmentException
 from ...options import OptionKey
 
@@ -48,7 +49,7 @@ class TaskingCompiler(Compiler):
     Functionality that is common to all TASKING family compilers.
     '''
 
-    LINKER_PREFIX = '-Wl'
+    LINKER_PREFIX = SimplePrefixLinkerOptionStyle('-Wl')
 
     def __init__(self) -> None:
         if not self.is_cross:

unittests/internaltests.py
@@ -28,6 +28,7 @@ import mesonbuild.modules.gnome
 import mesonbuild.scripts.env2mfile
 from mesonbuild import coredata
 from mesonbuild.compilers.c import ClangCCompiler, GnuCCompiler
+from mesonbuild.compilers.compilers import ManyInOneLinkerOptionStyle
 from mesonbuild.compilers.cpp import VisualStudioCPPCompiler
 from mesonbuild.compilers.d import DmdDCompiler
 from mesonbuild.compilers.detect import detect_c_compiler
@@ -323,7 +324,7 @@ class InternalTests(unittest.TestCase):
     def test_compiler_args_class_gnuld(self):
         ## Test --start/end-group
         env = get_fake_env()
-        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, '-Wl,', [])
+        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, ManyInOneLinkerOptionStyle('-Wl,', ','), [])
         gcc = GnuCCompiler([], [], 'fake', MachineChoice.HOST, env, linker=linker)
         ## Ensure that the fake compiler is never called by overriding the relevant function
         gcc.get_default_include_dirs = lambda: ['/usr/include', '/usr/share/include', '/usr/local/include']
@@ -353,7 +354,7 @@ class InternalTests(unittest.TestCase):
     def test_compiler_args_remove_system(self):
         ## Test --start/end-group
         env = get_fake_env()
-        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, '-Wl,', [])
+        linker = linkers.GnuBFDDynamicLinker([], env, MachineChoice.HOST, ManyInOneLinkerOptionStyle('-Wl,', ','), [])
         gcc = GnuCCompiler([], [], 'fake', MachineChoice.HOST, env, linker=linker)
         ## Ensure that the fake compiler is never called by overriding the relevant function
         gcc.get_default_include_dirs = lambda: ['/usr/include', '/usr/share/include', '/usr/local/include']