mesonbuild/meson

Add fallback for GCC when linker arguments can't be wrapped with -Wl

mesonbuild/compilers/mixins/gnu.py
@@ -17,7 +17,7 @@ import typing as T
 from ... import mesonlib
 from ... import mlog
 from ...options import OptionKey, UserStdOption
-from mesonbuild.compilers.compilers import CompileCheckMode, ManyInOneLinkerOptionWrapperStyle
+from mesonbuild.compilers.compilers import CompileCheckMode, ManyInOneLinkerOptionWrapperStyle, PrefixArgumentLinkerOptionWrapperStyle
 
 if T.TYPE_CHECKING:
     from ..._typing import ImmutableListProtocol
@@ -356,7 +356,8 @@ class GnuLikeCompiler(Compiler, metaclass=abc.ABCMeta):
     that the actual concrete subclass define their own implementation.
     """
 
-    LINKER_PREFIX = ManyInOneLinkerOptionWrapperStyle('-Wl,', ',')
+    LINKER_PREFIX = ManyInOneLinkerOptionWrapperStyle('-Wl,', ',',
+                                                      fallback=PrefixArgumentLinkerOptionWrapperStyle('-Xlinker'))
 
     def __init__(self) -> None:
         self.base_options = {

sthagen/mesonbuild-meson

mesonbuild/compilers/mixins/gnu.py
@@ -17,7 +17,7 @@ import typing as T
 from ... import mesonlib
 from ... import mlog
 from ...options import OptionKey, UserStdOption
-from mesonbuild.compilers.compilers import CompileCheckMode, ManyInOneLinkerOptionStyle
+from mesonbuild.compilers.compilers import CompileCheckMode, ManyInOneLinkerOptionStyle, PrefixArgumentLinkerOptionStyle
 
 if T.TYPE_CHECKING:
     from ..._typing import ImmutableListProtocol
@@ -356,7 +356,8 @@ class GnuLikeCompiler(Compiler, metaclass=mesonlib.SimpleABC):
     that the actual concrete subclass define their own implementation.
     """
 
-    LINKER_PREFIX = ManyInOneLinkerOptionStyle('-Wl,', ',')
+    LINKER_PREFIX = ManyInOneLinkerOptionStyle('-Wl,', ',',
+                                               fallback=PrefixArgumentLinkerOptionStyle('-Xlinker'))
 
     def __init__(self) -> None:
         self.base_options = {